81110aab5b1f410e56140984f26de131927532fc
Juan Chavez <>
2019-02-18 12:55:34 -0600

    add style sheet

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ let h1 = document.querySelector('h1')
 
 h1.style.color = 'red'
 h1.style.fontFamily = 'sans-serif'
+
+let textChange = document.querySelector('h2')
+textChange.addEventListener('click', function () {
+    console.log('h2 clicked')
+})
